0918a08c9a0539aa7fbb4738e9125887b7161c28
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2022-08-28 20:41:45 -0300

    Ajustando responsividades dos contents gerais e adicionando arquivo Resizer.js

---
+++ js/resizer.js
@@ -0,0 +1,35 @@
+const content = document.querySelector('.content');
+const contentAlpha = document.querySelector('.content-alpha');
+const contentBravo = document.querySelector('.content-bravo');
+const contentDelta = document.querySelector('.content-delta');
+const contentHeight = content.getBoundingClientRect().height;
+
+resizer();
+
+function resizer() {
+
+   var windowWidth = window.innerWidth;
+
+   var porcentWindowResizerdown = (windowWidth * 100) / 1325 - 100;
+
+   var porcentWindowResizer = porcentWindowResizerdown * -1;
+
+   var calcContentHeight = contentHeight * porcentWindowResizer / 100;
+
+   var formatContent = Math.trunc(calcContentHeight);
+
+   var newContentHeight = contentHeight - formatContent;
+
+
+   contentAlpha.style.height = `${newContentHeight}px`;
+   contentBravo.style.height = `${newContentHeight}px`;
+   contentDelta.style.height = `${newContentHeight}px`;
+
+}
+
+
+window.addEventListener('resize', function () {
+
+   resizer();
+
+});
