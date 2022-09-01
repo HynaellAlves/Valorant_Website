8c6825414545354bb21f0fa114e792ac07f5e1ba
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2022-09-01 20:28:00 -0300

    Comentários de código

--- js/Val-agents-carrossel.js
+++ js/Val-agents-carrossel.js
@@ -1,10 +1,14 @@
 // Script para funcionamento do carrossel
 
-// Importando arquivo com os objetos de agentes (Link do arquivo do agente e nome)
+/* 
+Importando arquivo com os objetos de agentes (Link do arquivo do agente e nome, 
+qualquer outra propriedade pode ser adicionada diretamente no arquivo de agentes) 
+*/
 
 import agents from "./agents-data.js";
 
 // Capturando elementos do carrossel
+
 const agent = document.querySelector('.agent');
 const nome_agent = document.querySelector('.nome-agent');
 
@@ -17,20 +21,39 @@ var i = 0;
 
 const loop = setInterval(() => {
 
+   // Recebendo valor dos objetos no arquivo de agentes de acordo com a posição da variável 'i'
+
+   // Nome do agente na posição 'i'
+
    nome = agents[i].nome;
+
+   // Caminho do agente na posição 'i'
+
    formatarCaminho = agents[i].src;
+
+   // Formatando caminho para que a propriedade css aceite o que será passado
+
    src = formatarCaminho.replace(formatarCaminho, '../Elementos/' + formatarCaminho);
 
-   //console.log(nome)
-   //console.log(src);
+   // Animações de transição
+
+   // Agente
 
    agent.classList.add('transition');
+
+   // Etiqueta do nome
+
    nome_agent.classList.add('transition');
+
+   // Tempo de espera para trocar o agente e o nome
 
    setTimeout(() => {
 
       agent.src = src;
       nome_agent.innerHTML = nome;
+
+      // Especificações particulares que alguns agentes necessitam para fins de estilo e posicionamento
+      // Foi necessário pois, esses agentes fugiram do padrão de imagem dos outros
 
       if (nome == 'Chamber') {
 
@@ -49,6 +72,9 @@ const loop = setInterval(() => {
 
    }, 2600)
 
+   // Intervalo para remover as classes de transição 
+   // que serão adicionadas novamente cada vez que o loop retornar ao início
+
    setTimeout(() => {
 
       agent.classList.remove('transition');
@@ -56,10 +82,11 @@ const loop = setInterval(() => {
 
    }, 2800);
 
-
-
+   // Alterando a posição da variável 'i' para a troca de agente
 
    i++
+
+   // Reiniciando o loop quando o contador, no caso o 'i' chegar ao último agente da lista
 
 
    if (i == agents.length) {
